4ef01cb3325769c391f32da0d672382453337c6b
Guilherme Simao <guilhermesimaoGH@outlook.com>
2023-05-20 19:30:38 -0300

    Changed Flask to Quart

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask, request, send_file, render_template
-from flask_cors import CORS
+from quart import Quart, request, send_file, render_template
+from quart_cors import cors
 import obj_detection_opencv
 import os
 import asyncio
 
-# Define Flask and CORS
-app = Flask(__name__)
-CORS(app)
+app = Quart(__name__)
+app = cors(app)
 
 temporary_files = []
 
@@ -16,53 +15,42 @@ async def process_image_async(image_path):
 
 
 @app.route('/')
-def index():
+async def index():
     index_path = 'index.html'
-    return render_template(index_path)
+    return await render_template(index_path)
 
 
 @app.route('/post-photo', methods=['POST'])
-def handle_image():
-    # Retrieving the POST image formData from frontend
-    image = request.files.get('image')
+async def handle_image():
+    image = await request.files.get('image')
 
-    # Return error if image wasn't in the formData
     if image is None:
         return 'Image not found in form', 400
 
-    # Setting the original image path variable
     original_image_path = os.getcwd() + '/my-photo.jpg'
 
-    # Saving the image locally and temporarily
-    image.save(original_image_path)
+    await image.save(original_image_path)
     temporary_files.append(original_image_path)
 
-    # Processing the image using the async function
     asyncio.create_task(process_image_async(original_image_path))
 
-    # Return the received image message
     return 'Image received successfully!', 200
 
 
-# Endpoint for sending the processed photo
 @app.route('/get-processed-photo', methods=['GET'])
 async def send_processed_photo_async():
-    # Setting processed image path
     processed_image_path = os.getcwd() + '/processed-photo.jpg'
 
-    # Adding processed image to temp array for deletion
     temporary_files.append(processed_image_path)
 
     while not os.path.exists(processed_image_path):
         await asyncio.sleep(0.1)
 
-    return send_file(processed_image_path, mimetype='image/jpg')
+    return await send_file(processed_image_path, mimetype='image/jpg')
 
 
-# Endpoint deleting images
 @app.route('/delete-files', methods=['DELETE'])
-def delete_files():
-    # Using OS to remove saved images
+async def delete_files():
     for file_path in temporary_files:
         os.remove(file_path)
 
