f77cb559f70877d58c2f78a5079e84a0e603383d
Guilherme Simao <guilhermesimaoGH@outlook.com>
2023-05-22 01:19:09 -0300

    Undo object_detection_opencv deletion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import cv2
-import numpy as np
 import os
+import obj_detection_opencv
 
 from flask import Flask, request, send_file
 from flask_cors import CORS
@@ -8,51 +7,6 @@ from flask_cors import CORS
 # Define Flask and CORS
 app = Flask(__name__)
 CORS(app)
-
-# Setting up openCV model
-config_file = os.getcwd() + '/opencvFiles/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-frozen_model = os.getcwd() + '/opencvFiles/frozen_inference_graph.pb'
-model = cv2.dnn_DetectionModel(frozen_model, config_file)
-
-# Reading the classes from model classes file
-class_Labels = np.array
-with open((os.getcwd() + '/opencvFiles/coco.names'), 'rt') as fpt:
-    class_Labels = fpt.read().rstrip('\n').split('\n')
-
-# Setting up color scheme
-np.random.seed(543210)
-colors = np.random.uniform(0, 255, size=(len(class_Labels), 3))
-
-# Configuring detection model input
-model.setInputSize(320, 320)
-model.setInputScale(1.0 / 127.5)
-model.setInputMean((127.5, 127.5, 127.5))
-model.setInputSwapRB(True)
-
-
-# Responsible for processing the image
-def process_image(image_path):
-    # Reads an image
-    img = cv2.imread(image_path)
-
-    if img is None or img.size == 0:
-        return f"Failed to load image: {image_path}"
-
-    # Detecting objects whose confidence values are above 55%
-    ClassIndex, confidence, bbox = model.detect(img, confThreshold=0.55)
-
-    # Setting up text style
-    font_scale = 2
-    font = cv2.FONT_HERSHEY_PLAIN
-
-    # Writing boxes and text of the detected objects in the image
-    for ClassInd, conf, boxes in zip(ClassIndex.flatten(), confidence.flatten(), bbox):
-        cv2.rectangle(img, boxes, colors[ClassInd], 2)
-        cv2.putText(img, class_Labels[ClassInd - 1].upper(), (boxes[0] + 10, boxes[1] + 40), font,
-                    fontScale=font_scale, color=colors[ClassInd], thickness=2)
-
-    # Saving image in specified folder
-    cv2.imwrite(os.getcwd() + '/processed-photo.jpg', img)
 
 
 # Responsible for receiving and saving image locally
@@ -69,7 +23,7 @@ def handle_image():
     image.save(os.getcwd() + '/my-photo.jpg')
 
     # Processing image
-    process_image(os.getcwd() + '/my-photo.jpg')
+    obj_detection_opencv.process_image(os.getcwd() + '/my-photo.jpg')
 
     # Return received image message
     return 'Image received successfully!', 200
